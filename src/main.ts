c0d6444dee51c3def59c88d215cd7113ecc61eaf
Jimy <jimyhoulbrook@gmail.com>
2023-08-29 22:23:56 +0100

    PIXI application created

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
-console.log('hello world');
+/** Name:   Jawbreaker main.ts
+ *  Desc:   Main file for the graph visualliser, will handle HTML inputs
+ *          and creation of tthe PIXI application for visualliser
+ *  Auth:   Jimy Houlbrook
+ *  Date:   29/08/2023
+ */
+
+import { Application } from 'pixi.js';
+
+export default class Main{
+
+    // Application to hold graph
+    private app : Application;
+
+    constructor(){
+        this.app = new Application<HTMLCanvasElement>({
+            height: 500,
+            width: 500,
+            hello: true,
+            view: <HTMLCanvasElement>document.getElementById('chart')
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => new Main);
