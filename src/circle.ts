b6e7b76085e35a95283f0b18b6ae56f1021840d2
Jimy <jimyhoulbrook@gmail.com>
2023-08-31 11:36:32 +0100

    dynamically changing circles
    Percent input now changes circle size
    todo: Color input

--- src/circle.ts
+++ src/circle.ts
@@ -14,6 +14,7 @@ export class Circle{
     public x : number;
     public y : number;
     public r : number;
+    public col: number;
 
     /** Constructor for circle
      * 
@@ -25,18 +26,16 @@ export class Circle{
      * @param r         Number      radius
      * @param canEdit   Boolean     can circle be edited
      */
-    constructor(x : number, y : number, r : number, col : number, percent : number = 0, canEdit : boolean = true){
+    constructor(x : number, y : number, r : number, col : number, bgArea : number, canEdit : boolean = true){
         this.graphics = new Graphics();
         
         this.x = x;
         this.y = y;
         this.r = r;
+        this.col = col;
 
         // Draw circle in background
-        this.graphics.beginFill(col);
-        this.graphics.drawCircle(this.x, this.y, this.r);
-        this.graphics.endFill();
-
+        this.drawCircle();
 
         // I am hoping that i will be acble
         // to just change the size of the circle
@@ -58,12 +57,21 @@ export class Circle{
         percDiv.id = "flexDiv"
         container.appendChild(percDiv);
 
+        // Calc percentage of area new circle takes up
+        let area : number = this.getArea();
+        let percent : number = Math.ceil((area / bgArea) * 100); 
+
         const percentInput : HTMLInputElement = document.createElement("input");
         percentInput.type = "number";
+        percentInput.min = "1";
+        percentInput.max = "100";
         percentInput.placeholder = percent.toString();
         percentInput.style.display = "flex";
         percentInput.maxLength = 2;
         percDiv.appendChild(percentInput);
+
+        // Resize on input
+        percentInput.addEventListener('input', () : void => this.resize(bgArea, percentInput))
 
         const percentSymbol : HTMLParagraphElement = document.createElement("p");
         percentSymbol.innerHTML = "%"
@@ -75,6 +83,35 @@ export class Circle{
         // parentDiv.appendChild(colorInput);
     }
 
+    private drawCircle() {
+        // Clear current graphics
+        this.graphics.clear();
+
+        // Draw new circle
+        this.graphics.beginFill(this.col);
+        this.graphics.drawCircle(this.x, this.y, this.r);
+        this.graphics.endFill();
+    }
+
+    private resize(bgArea : number, percIpt : HTMLInputElement) {
+        // Ensure input is between max and min value
+        let percent : number = isNaN(parseFloat(percIpt.value)) ? 0 : parseFloat(percIpt.value);
+
+        if (percent > 100){
+            percent = 100;
+            percIpt.value = "100";
+        } else if (percent < 0.1){
+            percent = 0.1;
+            percIpt.value = "0.1";
+        }
+        let area : number = (bgArea / 100) * percent;
+        this.r = Math.sqrt(area / Math.PI);
+        this.drawCircle();
+    }
+
+    public getArea() : number {
+        return Math.PI * Math.pow(this.r, 2);
+    }
 
     public getGraphics() : Graphics{
         return this.graphics;
--- src/main.ts
+++ src/main.ts
@@ -12,10 +12,18 @@ export default class Main{
 
     // Application to hold graph
     private app : Application;
+
+    // Array of circles within background
     private sections: Circle[];
+
+    // Background circle
     private bgCircle : Circle;
 
+    /** Constructor for main.ts
+     *  initialise global variables and initialise the webpage
+     */
     constructor(){
+        // Initialise globals
         this.app = new Application<HTMLCanvasElement>({
             height: 500,
             width: 500,
@@ -27,15 +35,19 @@ export default class Main{
 
         this.sections = new Array<Circle>;
 
+        // Create background circle
+        this.bgCircle = new Circle(
+            this.app.view.width / 2, this.app.view.height / 2, (this.app.view.width / 8) * 3, 0xFF0000, 0, false
+        );
+
         // Container to hold chart
         const container : Container = new Container();
         this.app.stage.addChild(container);
 
-        // Create background circle
-        this.bgCircle = new Circle(
-            this.app.view.width / 2, this.app.view.height / 2, (this.app.view.width / 8) * 3, 0xFF0000, 0, false);
+        // Add bgCircle to container
         container.addChild(this.bgCircle.getGraphics());
 
+        // Button for adding section to the chart
         const addSectionButton = <HTMLButtonElement>document.getElementById("addSection");
         addSectionButton.addEventListener('click', () => {
             let circle : Circle = this.createSection();
@@ -43,6 +55,13 @@ export default class Main{
         });
     }
 
+    /** createSection
+     *  
+     *  Creates a new section that is half the size of the last added section,
+     *  or half the size of the background
+     * 
+     * @returns Circle  The new circle that has been created.
+     */
     private createSection() : Circle{
 
         let lastCircle : Circle = this.sections.length === 0 ?
@@ -51,24 +70,18 @@ export default class Main{
         // Getting % of the radius does not create an accurate representation of
         // The percentage when shown in graph, therefore we are going to use
         // the area of the circle to create new sections
-        let lastArea : number = this.getArea(lastCircle.r);
+        let lastArea : number = lastCircle.getArea();
         let newArea : number = lastArea / 2;
         let radius : number = Math.sqrt(newArea / Math.PI);
 
-        // Get percentage of size that enw circle takes up
-        let percent : number = ( newArea / this.getArea(this.bgCircle.r)) * 100
+        let areaOfBg = this.bgCircle.getArea();
 
         let newCircle : Circle = new Circle(
-            this.app.view.width / 2, this.app.view.height / 2, radius, 0x008000, this.getArea(this.bgCircle.r));
+            this.app.view.width / 2, this.app.view.height / 2, radius, 0x008000, areaOfBg
+        );
 
         this.sections.push(newCircle);
         return newCircle
     }
-
-    // Get the area of cicle with radius r
-    private getArea(r : number) : number {
-        return Math.PI * Math.pow(r , 2);
-    }
 }
-
 document.addEventListener('DOMContentLoaded', () => new Main);
